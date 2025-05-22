3ec70fbd4b316be92a5eb50a4ff116be7e6a02de
Vasudev Innovative Software <83811058+vinnisoft@users.noreply.github.com>
2025-05-22 18:33:00 +0530

    Delete lib/logger directory

--- lib/logger/logger_utils.dart
+++
@@ -1,7 +0,0 @@
-import 'package:impact_zone/export.dart';
-
-class LoggerX {
-  static void write(String text, {bool isError = false}) {
-    Future.microtask(() => isError ? log.t(text) : log.i(text));
-  }
-}
